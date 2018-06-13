b9849c68b5578b1a3d934f4d1bf96ff7d9962643
Do Suh <doyoung.suh@gmail.com>
2018-06-12 22:40:04 -0700

    First Commit!

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+let myName = "Do Suh"
+let myAge = 30
+let myHometown = "San Francisco"
+var favoriteColor = "Blue"
+var favoriteFood = "🌯"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    var aboutMe: String {
+        get {
+            return """
+            Hello!  My name is \(myName) and I am \(myAge) years old.
+            My home town is \(myHometown) and I like the color \(favoriteColor).
+            If you come visit, we will feast on all of the \(favoriteFood)s together!
+            """
+            // triple quotes allow for multiline comments, can't comment within them though
+            }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func introduceMe(_ sender: Any) {
+        textView.text = aboutMe
     }
-
-
+    
+    @IBOutlet weak var textView: UITextView!
 }
 
